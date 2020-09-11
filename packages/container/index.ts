1cff53d5814da963ffafa42463b1595daaa2b3cd
Kitetop <33886685+Kitetop@users.noreply.github.com>
2020-09-11 15:57:52 +0800

    (fix): 修改单个组件安装的安装方式

--- packages/container/index.ts
+++ packages/container/index.ts
@@ -1,10 +1,11 @@
-import Container from './src/container';
 import ContainerVue from './src/container.vue'
 import Vue from 'vue';
+import { TsVueConstructor } from '../types/component';
 /**
  * 为了将来可以按需运用单个组件
  * @param vue : Vue
  */
+const Container = ContainerVue as TsVueConstructor;
 Container.install = (vue: typeof Vue) => {
     vue.component(Container.componentName, ContainerVue);
 }
