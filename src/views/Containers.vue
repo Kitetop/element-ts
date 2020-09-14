dafbb5b042b99171ec326ea2c2fe89e1566f9e06
Kitetop <33886685+Kitetop@users.noreply.github.com>
2020-09-14 17:54:19 +0800

    (update): 更新container布局的测试文件

--- src/views/Containers.vue
+++ src/views/Containers.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <el-ts-container direction="vertical" class="bg-purple-dark">
-      <div>test</div>
-      <div>test</div>
-      <div>test</div>
+    <el-ts-container>
+      <el-ts-header>Header</el-ts-header>
+      <el-ts-container>
+        <el-ts-aside width="200px">Aside</el-ts-aside>
+        <el-ts-main>Main</el-ts-main>
+      </el-ts-container>
+      <el-ts-footer>Footer</el-ts-footer>
+    </el-ts-container>
+    <hr />
+    <el-ts-container>
+      <el-ts-header>Header</el-ts-header>
+        <el-ts-main>Main</el-ts-main>
+      <el-ts-footer>Footer</el-ts-footer>
     </el-ts-container>
   </div>
 </template>
@@ -13,11 +22,33 @@ import Vue from "vue";
 import Component from "vue-class-component";
 
 @Component
-export default class Container extends Vue {}
+export default class Containers extends Vue {}
 </script>
+<style type="sass" scoped>
+.el-ts-header,
+.el-ts-footer {
+  background-color: #b3c0d1;
+  color: #333;
+  text-align: center;
+  line-height: 60px;
+}
 
-<style type="sass">
-.bg-purple-dark {
-  background: #99a9bf;
+.el-ts-aside {
+  background-color: #d3dce6;
+  color: #333;
+  text-align: center;
+  line-height: 200px;
+}
+
+.el-ts-main {
+  background-color: #e9eef3;
+  color: #333;
+  text-align: center;
+  line-height: 160px;
+}
+
+body > .el-ts-container {
+  margin-bottom: 40px;
 }
 </style>
+
