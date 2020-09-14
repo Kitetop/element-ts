4dafe107e8df8c74c2a4df1239d1a0098809dd31
Kitetop <33886685+Kitetop@users.noreply.github.com>
2020-09-14 17:55:48 +0800

    (update): 更新el-ts包入口文件，新增部分组件

--- packages/index.ts
+++ packages/index.ts
@@ -1,13 +1,21 @@
 import Vue from 'vue'
 import Row from './row/src/row'
 import Col from './col/src/col';
-// import Container from './container/src/container.vue';
-// import { TsVueConstructor } from './types/component';
+import Container from './container/src/container.vue';
+import Header from './header/src/header.vue';
+import { TsVueConstructor } from './types/component';
+import Footer from './footer/src/footer';
+import Main from './main/src/mian';
+import Aside from './aside/src/aside';
 
 const components = [
     Row,
     Col,
-    // Container as TsVueConstructor
+    Container as TsVueConstructor,
+    Header as TsVueConstructor,
+    Footer,
+    Main,
+    Aside
 ];
 
 /** 装载所有的组件 */
@@ -21,7 +29,10 @@ if (typeof window != 'undefined' && window.Vue) {
     install(window.Vue);
 }
 export default {
+    install,
     Row,
     Col,
-    install
+    Container,
+    Header,
+    Footer,
 }
